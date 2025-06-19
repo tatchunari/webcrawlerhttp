b0307db25768139a9e426b5f5bfb8137664b9200
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-06-19 13:40:14 +0700

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,33 @@
+const {JSDOM} = require('jsdom');
+
+
+function getURLsFromHTML(htmlBody, baseURL){
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll('a');
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0,1) === '/') {
+      // relative
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with relative urls: ${err.message}`)
+      }
+    } else {
+      // absolute
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log(`error with absolute url: ${err.message}`)
+      }
+    }
+  }
+  return urls;
+};
+
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -9,5 +39,6 @@ function normalizeURL(urlString) {
 };
 
 module.exports = {
-  normalizeURL
+  normalizeURL,
+  getURLsFromHTML
 };
